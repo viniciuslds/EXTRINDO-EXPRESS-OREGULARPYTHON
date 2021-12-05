96f6d90adebfd72b2b83035166da6428884dcc3e
viniciuslds <viniciuslimadasilva4@gmail.com>
2021-12-05 19:36:20 -0300

    Dyenefer

---
+++ extrdados.py
@@ -0,0 +1,12 @@
+import urllib
+import requests
+import re
+
+page = "https://noticias.uol.com.br/ultimas/"
+html = requests.get(page).text
+lista = re.findall(r'<h3 class="thumb-title title-xsmall title-lg-small"[^>]*>(.*?)</h3>',html)
+noticias=''
+for i in lista:
+    noticias+=i+'\n\n'
+
+print(noticias)
